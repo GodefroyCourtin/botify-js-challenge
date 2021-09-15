bc295b5aeb528492bd3075457401eb7f98130b88
Godefroy <courtin.godefroy@gmail.com>
2021-09-15 18:56:00 +0200

    npm i react-google-charts and display the first element of neos state (array)

--- src/components/BarChartNeos.js
+++ src/components/BarChartNeos.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Chart from "react-google-charts";
 
 const BarChartNeos = () => {
   const [neos, setNeos] = useState([]);
@@ -28,14 +29,36 @@ const BarChartNeos = () => {
 
   return (
     <div>
-      <p>
-        {neos[0].name}
-        {neos[0].estimated_diameter_max}
-      </p>
-      <p>
-        {neos[1].name}
-        {neos[0].estimated_diameter_min}
-      </p>
+      {neos.length > 0 && (
+        <Chart
+          width={"1000px"}
+          height={"600px"}
+          chartType="BarChart"
+          loader={<div>Loading Chart</div>}
+          data={[
+            ["Name", "estimated diameter max", "estimated diameter min"],
+            [
+              neos[0].name,
+              neos[0].estimated_diameter_max,
+              neos[0].estimated_diameter_min,
+            ],
+          ]}
+          options={{
+            title:
+              "min and max estimated diameter, sorted by average estimated diameter descending",
+            chartArea: { width: "50%" },
+            hAxis: {
+              itle: "min and max estimated diameter",
+              minValue: 0,
+            },
+            vAxis: {
+              title: "NEO name",
+            },
+          }}
+          // For tests
+          rootProps={{ "data-testid": "1" }}
+        />
+      )}
     </div>
   );
 };
