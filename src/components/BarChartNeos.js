08ec625722349c8229be314e1c44a62e52b5e49d
Godefroy <courtin.godefroy@gmail.com>
2021-09-15 20:41:43 +0200

    update title graph ok

--- src/components/BarChartNeos.js
+++ src/components/BarChartNeos.js
@@ -16,7 +16,7 @@ const BarChartNeos = () => {
       setIsLoading(true);
       try {
         const response = await axios.get(
-          `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${process.env.REACT_APP_API_KEY_NASA}`
+          `https://www.neowsapp.com/rest/v1/neo/browse?page=0&size=20&api_key=${process.env.REACT_APP_API_KEY_NASA}`
         );
         const data = response.data.near_earth_objects.map((neo) => ({
           name: neo.name,
@@ -24,6 +24,9 @@ const BarChartNeos = () => {
             neo.estimated_diameter.kilometers.estimated_diameter_max,
           estimated_diameter_min:
             neo.estimated_diameter.kilometers.estimated_diameter_min,
+          closeApproaches: neo.close_approach_data.map((approach) => ({
+            orbitingBody: approach.orbiting_body,
+          })),
         }));
         setIsLoading(false);
         // set new state with data we have just fetched
@@ -67,7 +70,7 @@ const BarChartNeos = () => {
           data={formatData}
           options={{
             title:
-              "min and max estimated diameter, sorted by average estimated diameter descending",
+              "NEOS min and max estimated diameter, sorted by average estimated diameter descending",
             chartArea: { width: "50%" },
             hAxis: {
               itle: "min and max estimated diameter",
